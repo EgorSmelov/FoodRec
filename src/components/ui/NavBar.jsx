ced717418236c845e6f9a9a25cbe3330121cfd9c
EgorSmelov <smelov909@mail.ru>
2023-12-14 18:20:46 +0300

    navbar+profile

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-export default function NavBar() {
+export default function NavBar({ user }) {
   return (
-    <div>
-    </div>
-  )
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Книга рецептов</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link active" aria-current="page" href="/">На главную</a>
+            <a style={{ display: `${!user ? 'none' : 'block'}` }} className="nav-link" aria-current="page" href={user ? '/auth/profile' : '/#'}>Профиль</a>
+            <a style={{ display: `${user ? 'none' : 'block'}` }} className="nav-link" aria-current="page" href="/auth/reg">Регистрация</a>
+            <a className="nav-link" aria-current="page" href="/auth/login">Войти</a>
+            <a className="nav-link" aria-current="page" href="/auth/logout">Выйти</a>
+            <a className="nav-link" aria-current="page" href={user ? '/auth/profile' : '/#'}>{user ? `${user.name}` : 'Гость'}</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
